66e2ab3f1d251849d8cb38412ce2eeb9f30450ad
shehapkhalil62@gmail.com <shehapkhalil62@gamil.com>
2023-11-12 02:46:50 -0800

    edit

--- _printf.c
+++ _printf.c
@@ -1,54 +1,63 @@
 #include "main.h"
 
-handler_t* create_handlers() {
-    static handler_t handlers[] = {
-        {'c', handle_char},
-        {'s', handle_string},
-        {'%', handle_percent},
-        {'d', handle_int},
-        {'i', handle_int},
-        {'\0', NULL}
-    };
-    return handlers;
+/**
+ * create_handlers - create handlers
+ * Return:handlers
+ */
+handler_t *create_handlers(void)
+{
+	static handler_t handlers[] = {
+		{'c', handle_char},
+		{'s', handle_string},
+		{'%', handle_percent},
+		{'d', handle_int},
+		{'i', handle_int},
+		{'\0', NULL}
+	};
+	return (handlers);
 }
 
-int _printf(const char *format, ...) {
-    va_list args;
-    int count;
-    char *buffer;
-    const char *p = format;
-    handler_t *h;
-    handler_t *handlers;
+/**
+ * _printf - print
+ * @format:the string
+ * Return:number of printed char
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count;
+	char *buffer;
+	const char *p = format;
+	handler_t *h;
+	handler_t *handlers;
 
-
-    va_start(args,format);
-
-    handlers = create_handlers();
-
-    buffer = malloc(1024 * sizeof(char));
-    if (buffer == NULL) {
-        return -1;
-    }
-    count = 0;
-    for (; *p != '\0'; p++) {
-        if (*p != '%') {
-            buffer[count++] = *p;
-            continue;
-        }
-
-        p++;
-
-        for (h = handlers; h->specifier != '\0'; h++) {
-            if (h->specifier == *p) {
-                h->handler(args, &buffer, &count);
-                break;
-            }
-        }
-    }
-
-    write(1, buffer, count);
-    free(buffer);
-
-    va_end(args);
-    return count;
+	va_start(args, format);
+	handlers = create_handlers();
+	buffer = malloc(1024 * sizeof(char));
+	if (buffer == NULL)
+	{
+		return (-1);
+	}
+	count = 0;
+	for (; *p != '\0'; p++)
+	{
+		if (*p != '%')
+		{
+			buffer[count++] = *p;
+			continue;
+		}
+		p++;
+		for (h = handlers; h->specifier != '\0'; h++)
+		{
+			if (h->specifier == *p)
+			{
+				h->handler(args, &buffer, &count);
+				break;
+			}
+		}
+	}
+	write(1, buffer, count);
+	free(buffer);
+	va_end(args);
+	return (count);
 }
--- function.c
+++ function.c
@@ -1,54 +1,100 @@
 #include "main.h"
 
-void handle_char(va_list args, char** buffer, int* count) {
-    char c = va_arg(args, int);
-    (*buffer)[(*count)++] = c;
+/**
+ * handle_char - print the char
+ * @args:arguments
+ * @buffer: buffer hold string
+ * @count: num of char printed
+ * Return:void
+ */
+void handle_char(va_list args, char **buffer, int *count)
+{
+	char c = va_arg(args, int);
+	(*buffer)[(*count)++] = c;
 }
 
-void handle_string(va_list args, char** buffer, int* count) {
-    char *s = va_arg(args, char *);
-    char *sp;
+/**
+ * handle_string - print the string
+ * @args:arguments
+ * @buffer: buffer hold string
+ * @count: num of char printed
+ * Return:void
+ */
+void handle_string(va_list args, char **buffer, int *count)
+{
+	char *s = va_arg(args, char *);
+	char *sp;
 
-    for (sp = s; *sp != '\0'; sp++) {
-        (*buffer)[(*count)++] = *sp;
-    }
+	for (sp = s; *sp != '\0'; sp++)
+	{
+		(*buffer)[(*count)++] = *sp;
+	}
 }
 
-void handle_percent(va_list args, char** buffer, int* count) {
-    	(void)args;
+/**
+ * handle_percent - print the %
+ * @args:arguments
+ * @buffer: buffer hold string
+ * @count: num of char printed
+ * Return:void
+ */
+void handle_percent(va_list args, char **buffer, int *count)
+{
+	(void)args;
+
 	(*buffer)[(*count)++] = '%';
 }
 
-void itoa(int value, char* str) {
-    char* start = str;
-    int is_negative = 0;
-    char temp;
-    if (value < 0) {
-        is_negative = 1;
-        value = -value;
-    }
-    do {
-        *str++ = '0' + (value % 10);
-        value /= 10;
-    } while (value != 0);
-    if (is_negative) {
-        *str++ = '-';
-    }
-    *str-- = '\0';
-    while (start < str) {
-        temp = *start;
-        *start++ = *str;
-        *str-- = temp;
-    }
-}
+/**
+ * itoa - convert int to string
+ * @value:the number
+ * @str:the string
+ * Return:void
+ */
+void itoa(int value, char *str)
+{
+	char *start = str;
+	int is_negative = 0;
+	char temp;
 
-void handle_int(va_list args, char** buffer, int* count) {
-    int value = va_arg(args, int);
-    char str[12];
-    char *sp;
-    itoa(value, str);
-    for (sp = str; *sp != '\0'; sp++) {
-        (*buffer)[(*count)++] = *sp;
-    }
+	if (value < 0)
+	{
+		is_negative = 1;
+		value = -value;
+	}
+	do {
+		*str++ = '0' + (value % 10);
+		value /= 10;
+	} while (value != 0);
+	if (is_negative)
+	{
+		*str++ = '-';
+	}
+	*str-- = '\0';
+	while (start < str)
+	{
+		temp = *start;
+		*start++ = *str;
+		*str-- = temp;
+	}
+}
+/**
+ * handle_int - print the num
+ * @args:arguments
+ * @buffer: buffer hold string
+ * @count: num of char printed
+ * Return:void
+ */
+void handle_int(va_list args, char **buffer, int *count)
+{
+	int value = va_arg(args, int);
+	char str[12];
+	char *sp;
+
+	itoa(value, str);
+	for (sp = str; *sp != '\0'; sp++)
+	{
+		(*buffer)[(*count)++] = *sp;
+	}
 }
 
